5456a9c774f18eca887d363ffb5746c0e9a5dcae
Phenoo <descometusah@gmail.com>
2024-08-20 04:24:58 +0100

    Initial Commit

---
+++ SleepOptimize/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SleepOptimize
+//
+//  Created by Eze Chidera Paschal on 20/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SleepOptimize/SleepOptimizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepOptimizeApp.swift
+//  SleepOptimize
+//
+//  Created by Eze Chidera Paschal on 20/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SleepOptimizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
